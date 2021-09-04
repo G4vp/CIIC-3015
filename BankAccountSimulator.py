659ee4dfd971a544d9473aa7d739504f59c51f0c
Gabriel Viera <gviera2442@gmail.com>
2021-09-03 21:46:34 -0400

    v0.1.6 : A record system was added.

--- BankAccountSimulator.py
+++ BankAccountSimulator.py
@@ -1,8 +1,11 @@
+from datetime import datetime
+
 class bcolors:
     green = '\033[92m'
     yellow = '\033[93m'
     red = '\033[91m'
     black = '\033[0m'
+    OKCYAN = '\033[96m'
 
 
 account_name = input('Enter an account name: ').strip()
@@ -22,6 +25,8 @@ count_withdraw = 0
 count_penalty = 0
 count_deposit = 0
 
+record_withdraw = []
+record_deposit = []
 
 def stop():
     print(f'\nFinal balance: ${balance:,.2f}')
@@ -59,6 +64,7 @@ def choice_w():
                 balance -= w_amount
                 total_withdraw += w_amount
                 count_withdraw += 1
+                record_withdraw.append([f'-${w_amount:,.2f}',datetime.now().isoformat(sep='|', timespec='minutes')])
             else:
                 print(f'{bcolors.red}\nUnsuccessful withdrawal. Insufficient funds. $5 have been deducted from your '
                     f'account.{bcolors.black}')
@@ -82,6 +88,7 @@ def choice_d():
             balance += d_amount
             total_deposit += d_amount
             count_deposit += 1
+            record_deposit.append([f'${d_amount:,.2f}',datetime.now().isoformat(sep='|', timespec='minutes')])
         else:
             print(f'{bcolors.yellow}\nPlease enter a valid input.{bcolors.black}')
             choice_d()
@@ -89,26 +96,60 @@ def choice_d():
         print(f'{bcolors.yellow}\nPlease enter a valid input.{bcolors.black}')
         choice_d()
 
+def choice_r():
+    global record_deposit,total_deposit,total_withdraw
+    print(f'\n------------------\n'
+    f'RECORD OF DEPOSITS\n'
+    f'------------------\n')
+    record_choice=input(f'Which record would you like to see?\n\n'
+                        f"1) Withdraw's Record\n"
+                        f"2) Deposit's Record\n"
+                        f"3) Go Back\n\n"
+                        f'Please enter your selection: ').strip().lower()
+    if record_choice in ('1', "Withdraw's Record"):
+        if(len(record_withdraw) == 0):
+            print(f"{bcolors.red}\n-No withdraw's record available-{bcolors.black}")
+        else:
+            for x in record_withdraw:
+                print(f"\n{bcolors.yellow}{x[0]}  |  {x[1]}{bcolors.black}")
+            print(f"{bcolors.yellow}\nTOTAL: -${total_withdraw:,.2f}{bcolors.black}")
+            input("\nPress any key to continue.")
+    elif record_choice in ('2', "Deposit's Record"):
+        if(len(record_deposit) == 0):
+            print(f"{bcolors.red}\n-No deposit's record available-{bcolors.black}")
+        else:
+            for x in record_deposit:
+                print(f"\n{bcolors.OKCYAN}{x[0]}  |  {x[1]}{bcolors.black}")
+            print(f"{bcolors.OKCYAN}\nTOTAL: ${total_deposit:,.2f}{bcolors.black}")
+            input("\nPress any key to continue.")
+    elif record_choice in ('3', 'Go Back'):
+        return
+    else:
+        print(f'{bcolors.yellow}\nPlease enter a valid input.{bcolors.black}')
+    choice_r()
 
 def choice():
     user_choice = input(f'\nWhat would you like to do?\n\n'
                         f'1) Withdraw\n'
                         f'2) Deposit\n'
-                        f'3) Quit\n\n'
+                        f'3) Record\n'
+                        f'4) Quit\n\n'
                         f'Please enter your selection: ').strip().lower()
     if user_choice in ('1', 'w', 'withdraw'):
         choice_w()
-        info()
-        choice()
+
     elif user_choice in ('2', 'd', 'deposit'):
         choice_d()
-        info()
-        choice()
-    elif user_choice in ('3', 'q', 'quit'):
-        stop()
+
+    elif user_choice in ('3', 'r', 'record'):
+        choice_r()
+
+    elif user_choice in ('4', 'q', 'quit'):
+        return stop()
     else:
         print(f'{bcolors.yellow}\nPlease enter a valid input.{bcolors.black}')
-        choice()
+    info()
+    choice()
 
 
 info()
