53470e64035fff15af169074d188e1590558d034
Gabriel Viera <gviera2442@gmail.com>
2021-10-16 18:02:11 -0400

    New Project

---
+++ Text_Adventure/Text_Adventure_Game.py
@@ -0,0 +1,27 @@
+# CIIC 3015 PROJECT 2
+# Gabriel A. Viera and Jose R Rivera 
+
+
+def houseMap():
+    house = (
+        f'         -----------      \n'
+        f'         |   Bed   |       \n'
+        f'         |   Room  |       \n'
+        f'_________|__     __|_________\n'
+        f'|        |         |        |\n'
+        f'H  Front   Living    Office |\n'
+        f'H  Door                     |\n'
+        f'|________|__     __|________|\n'
+        f'         |         |         \n'
+        f'         | Kitchen |         \n'
+        f'         |         |         \n'
+        f'         -----------         \n'         
+    )
+
+    print(house)
+
+
+def main():
+    houseMap()
+
+main()
